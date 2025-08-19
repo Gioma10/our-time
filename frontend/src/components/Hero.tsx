5aac8ce12d25546d79e94a1f0c76beca84ce59f7
Giovanni Mauro <giovi.mauro03@gmail.com>
2025-08-19 20:18:06 +0200

    import fonts from dafont

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -2,7 +2,7 @@ const Hero: React.FC = () => {
   return (
     <div className="h-screen flex items-center justify-center">
       <div className="border text-center">
-        <h1>Welcome</h1>
+        <h1 className="text-8xl">Welcome</h1>
         <span>Your connection to every corner of the clock.</span>
       </div>
     </div>
--- frontend/src/components/SwitcherMode.tsx
+++ frontend/src/components/SwitcherMode.tsx
@@ -8,14 +8,14 @@ const SwitcherMode: React.FC = () => {
 
   return (
     <button
-      className={`w-24 h-10 rounded-4xl bg-white shadow-md  ${isDark ? 'shadow-white' : 'shadow-black'} cursor-pointer relative`}
+      className={`w-24 h-10 rounded-4xl bg-white shadow-lg cursor-pointer relative`}
       onClick={toggleTheme}
     >
       {/* circle  */}
       <motion.div
         animate={{ x: isDark ? 48 : 0 }}
         transition={{duration: 0.5}}
-        className={`shadow-lg shadow-black w-1/2 h-full rounded-full bg-white border-2 border-transparent absolute bottom-0`}
+        className={`shadow-sm shadow-black w-1/2 h-full rounded-full bg-white border-2 border-transparent absolute bottom-0`}
       ></motion.div>
 
       <div className="flex justify-around text-black">
